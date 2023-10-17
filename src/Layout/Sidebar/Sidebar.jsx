e5c9955d5040fc89ed93973b9e7dd40abb71ad5b
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-10-17 23:29:53 +0530

    Added Modals in the Buttons.

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,15 +1,52 @@
 import React, { useState } from "react"
 import "./Sidebar.css"
-import { Avatar, Typography, Select, MenuItem, InputLabel, FormControl, Button } from "@mui/material"
+import { Avatar, Typography, Select, MenuItem, InputLabel, Box, FormControl, Button, Modal } from "@mui/material"
 
 export default function Sidebar() {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  }
+
   var names = ["Project 1", "Project 2", "Project 3", "Project 4", "Project 5"]
   const [projectName, setProjectName] = useState("Project 1")
+  const [contactModal, setContactModal] = useState(false)
+  const [analyticModal, setAnalyticModal] = useState(false)
+  const [sendReportModal, setSendReportModal] = useState(false)
 
-  const handleProjectChange=(e)=>{
-    console.log("EHl")
-      setProjectName(e.target.value)
+  const openContactModal = () => {
+    setContactModal(true)
   }
+
+  const closeContactModal = () => {
+    setContactModal(false)
+  }
+  const openAnalytictModal = () => {
+    setContactModal(true)
+  }
+
+  const closeAnalyticModal = () => {
+    setContactModal(false)
+  }
+  const openSendReportModal = () => {
+    setContactModal(true)
+  }
+
+  const closeSendReportModal = () => {
+    setContactModal(false)
+  }
+
+  const handleProjectChange = (e) => {
+    setProjectName(e.target.value)
+  }
+
   return (
     <div className="sidebarContainer">
       <div className="sidebarProfile">
@@ -19,7 +56,14 @@ export default function Sidebar() {
       <div className="sidebarProjects">
         <FormControl>
           <InputLabel id="demo-multiple-name-label">Select Project</InputLabel>
-          <Select labelId="demo-multiple-name-label" id="demo-multiple-name" value={projectName} label="Project Name" style={{ minWidth: 200 }} onChange={(e)=>handleProjectChange(e)}>
+          <Select
+            labelId="demo-multiple-name-label"
+            id="demo-multiple-name"
+            value={projectName}
+            label="Project Name"
+            style={{ minWidth: 200 }}
+            onChange={(e) => handleProjectChange(e)}
+          >
             {names.map((name) => (
               <MenuItem key={name} value={name}>
                 {name}
@@ -29,11 +73,55 @@ export default function Sidebar() {
         </FormControl>
       </div>
       <div className="sidebarOptions">
-      <Button variant="contained" className="standardBtn" style={{width:200,marginBottom:10,height:50}}>Check Analytics</Button>
-      <Button variant="contained" className="standardBtn" style={{width:200,marginBottom:10,height:50}}>Download Report</Button>
-      <Button variant="contained" className="standardBtn" style={{width:200,marginBottom:10,height:50}}>Send Report</Button>
-      <Button variant="contained" className="standardBtn" style={{width:200,marginBottom:10,height:50}}>Add Contacts</Button>
+        <Button variant="contained" className="standardBtn" onClick={openAnalytictModal} style={{ width: 200, marginBottom: 10, height: 50 }}>
+          Check Analytics
+        </Button>
+        <Button variant="contained" className="standardBtn" style={{ width: 200, marginBottom: 10, height: 50 }}>
+          Download Report
+        </Button>
+        <Button variant="contained" className="standardBtn" onClick={openSendReportModal} style={{ width: 200, marginBottom: 10, height: 50 }}>
+          Send Report
+        </Button>
+        <Button variant="contained" className="standardBtn" onClick={openContactModal} style={{ width: 200, marginBottom: 10, height: 50 }}>
+          Add Contacts
+        </Button>
       </div>
+
+      {/* Contact Modal */}
+      <Modal open={contactModal} onClose={closeContactModal}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
+      {/* Check Analytics */}
+      <Modal open={analyticModal} onClose={closeAnalyticModal}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+
+      {/* Send Report */}
+      <Modal open={sendReportModal} onClose={closeSendReportModal}>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
     </div>
   )
 }
