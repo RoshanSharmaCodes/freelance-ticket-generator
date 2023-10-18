83d6095d0ad21d07273f7e00f24ffe0771256b3f
Roshan Kumar Sharma <roshan.sharma@deprontoinfotech.com>
2023-10-18 14:51:43 +0530

    Modal Fields Added

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react"
 import "./Sidebar.css"
-import { Avatar, Typography, Select, MenuItem, Input, InputLabel, Box, FormControl, Button, Modal, FormHelperText } from "@mui/material"
+import { Avatar, Typography, Select, MenuItem, Input, InputLabel, Box, FormControl, Button, Modal, FormHelperText, TextField } from "@mui/material"
 import { DatePicker } from "@mui/x-date-pickers"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import Paper from "@mui/material/Paper"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
 
 export default function Sidebar() {
   const style = {
@@ -11,12 +18,24 @@ export default function Sidebar() {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: 700,
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
     p: 4,
   }
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein }
+  }
+
+  const rows = [
+    createData("Creating Layout", "Done", 6.0, 24),
+    createData("Login Form", "WIP", 9.0, 37),
+    createData("Registration Form", "Done", 16.0, 24),
+    createData("State Management", "Done", 3.7, 67),
+    createData("Creating Modal", "Done", 16.0, 49),
+  ]
 
   var names = ["Project 1", "Project 2", "Project 3", "Project 4", "Project 5"]
   const [projectName, setProjectName] = useState("Project 1")
@@ -115,24 +134,49 @@ export default function Sidebar() {
       {/* Check Analytics */}
       <Modal open={analyticModal} onClose={closeAnalyticModal}>
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Task Name</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                  <TableCell align="right">Hours</TableCell>
+                  <TableCell align="right">Cost&nbsp;($)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.name} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.calories}</TableCell>
+                    <TableCell align="right">{row.fat}</TableCell>
+                    <TableCell align="right">{row.carbs}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="totalAnalyticsDiv">
+          <InputLabel htmlFor="project-total-task">Total Tasks :</InputLabel>
+          <InputLabel htmlFor="project-total-hours">Total Hours :</InputLabel>
+          <InputLabel htmlFor="project-name-input">Total Cost :</InputLabel>
+          </div>
         </Box>
       </Modal>
 
       {/* Send Report */}
       <Modal open={sendReportModal} onClose={closeSendReportModal}>
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <InputLabel id="demo-simple-select-label">Client's Name</InputLabel>
+          <Select labelId="demo-simple-select-label" id="demo-simple-select" value="Harshil" label="Age">
+            <MenuItem value={"Harshil"}>Harshil</MenuItem>
+            <MenuItem value={"Vipul"}>Vipul</MenuItem>
+            <MenuItem value={"Harman"}>Harman</MenuItem>
+          </Select>
+          <InputLabel htmlFor="project-name-input">Message</InputLabel>
+          <TextField id="outlined-multiline-static" multiline rows={4} />
         </Box>
       </Modal>
 
