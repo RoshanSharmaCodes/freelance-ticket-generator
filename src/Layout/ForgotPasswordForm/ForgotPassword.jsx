a3abbd9e98c9c149a742397fdea8a5279599f104
Roshan Kumar Sharma <roshan.rks2812000@gmail.com>
2023-11-18 16:04:38 +0530

    Resend Button UI.

--- src/Layout/ForgotPasswordForm/ForgotPassword.jsx
+++ src/Layout/ForgotPasswordForm/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import "./ForgotPassword.css"
 import { Avatar, Button, Chip, Divider, TextField } from "@mui/material"
 import { Link, useNavigate } from "react-router-dom"
@@ -7,23 +7,46 @@ import { Label } from "@mui/icons-material"
 export default function ForgotPasswordForm() {
   const navigate = useNavigate()
   const [elementState, setElementState] = useState(true)
+  const [timer, setTimer] = useState(60)
   
   const handleSendOTP = () => {
     setElementState(false)
+    document.getElementsByClassName("resendOTPDiv")[0].style.display = "block"
   }
+
 
   const handleVerifyOTP = () =>{
       navigate("/ConfirmPassword")
   }
+
+  const handleResendOTP = () => {
+    setTimer(60)
+  }
+
+  useEffect(()=>{
+    let timeInterval = setTimeout(()=>{
+      console.log("Timer is running")
+      if(timer<=0) return
+      setTimer(timer-1)
+    },1000)
+    return ()=> clearInterval(timeInterval)
+  },[timer])
 
   return (
     <div className="forgotContainer">
       <div className="forgotMainForm">
         <div className="forgotMain">
           <TextField id="standard-basic" label="Type Your Email" variant="outlined" style={{marginBottom: 10}} />
-          <Button variant="contained" onClick={handleSendOTP} color="success" style={{marginBottom: 20}}> 
+          <Button variant="contained" onClick={handleSendOTP} color="success"> 
             Sent OTP
           </Button>
+          <div className="resendOTPDiv" style={{marginBottom: 20, display:"none"}}>
+          {timer<=0? <span onClick={handleResendOTP} style={{ fontSize: 13, color: "#199be6", marginTop: "30px", cursor: "pointer"}}>
+            Resend OTP
+          </span>: <span style={{ fontSize: 13, color: "grey", marginTop: "30px" }}>
+            Resend OTP? ({timer})
+          </span>}
+        </div>
           <TextField id="standard-basic" label="OTP" variant="outlined"style={{marginBottom: 10}} disabled={elementState}/>
           <Button variant="contained" onClick={handleVerifyOTP} color="success" style={{marginBottom: 20}} disabled={elementState}> 
             Verify
